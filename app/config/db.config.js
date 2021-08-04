3a1bd102e0e3c3c4e1f67d8e329da8af40e5ff57
BFC DEVELOPERS <78731357+bfc-developer@users.noreply.github.com>
2021-08-04 15:33:16 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,21 +1,7 @@
 module.exports = {
-  HOST: "162.241.124.25",
-  USER: "bfcca9qh_mfprodigys",
+  HOST: "162.241.124.25sssssssssssssss",
+  USER: "bfcca9qh_mfprodigy",
   PASSWORD: "W.fA.WaIC*kG",
   DB: "bfcca9qh_mfprodigy"
 };
-
-// module.exports = {
-//   HOST: "162.215.252.35",
-//   USER: "trishffe_prodigy",
-//   PASSWORD: "C@ZpF^MB_D2M",
-//   DB: "trishffe_prodigy_db"
-// };
-
-// module.exports = {
-//   HOST: "localhost",
-//   USER: "root",
-//   PASSWORD: "",
-//   DB: "test" 
-// };
 
